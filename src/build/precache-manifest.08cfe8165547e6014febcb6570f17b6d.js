3dd27f8cb5cc689f9255d3cd5dd03a61d1e127bc
Tony N <tonyxn0422@gmail.com>
2019-06-21 15:04:34 -0400

    First commit

---
+++ src/build/precache-manifest.08cfe8165547e6014febcb6570f17b6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baaf071554faa0747f9aeba31ade2f4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d6f2489dd1a22e6461d",
+    "url": "/static/css/main.73ff5b42.chunk.css"
+  },
+  {
+    "revision": "bb660b66d97ca872da68",
+    "url": "/static/js/2.ad55af6c.chunk.js"
+  },
+  {
+    "revision": "4d6f2489dd1a22e6461d",
+    "url": "/static/js/main.f3a523a5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
